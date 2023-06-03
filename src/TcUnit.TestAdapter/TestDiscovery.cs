4aafa0a06673eb421540c40ead83d4c0f3dfe4ed
philippleidig <noreplay@github.com>
2023-06-03 15:57:12 +0200

    initial commit

---
+++ src/TcUnit.TestAdapter/TestDiscovery.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TcUnit.TestAdapter
+{
+    [FileExtension(".tsproj")]
+    [DefaultExecutorUri(TestExecutor.ExecutorUriString)]
+    public class TestDiscovery : ITestDiscoverer
+    {
+
+        // C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\TestPlatform\Extensions
+        // C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\Extensions\TestPlatform\Extensions
+        public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+        {
+            try
+            {
+                logger.SendMessage(TestMessageLevel.Informational, "Discover Tests");
+                
+                foreach (var source in sources)
+                {
+                    logger.SendMessage(TestMessageLevel.Informational, source);
+                }
+
+                GetTests(sources, discoverySink);
+
+                logger.SendMessage(TestMessageLevel.Informational, "Tests discovered");
+
+            }
+            catch (Exception ex)
+            {
+                logger.SendMessage(TestMessageLevel.Error, ex.Message);
+            }
+        }
+
+        internal static IEnumerable<TestCase> GetTests(IEnumerable<string> sources, ITestCaseDiscoverySink discoverySink)
+        {
+            var tests = new List<TestCase>();
+
+            foreach (var source in sources)
+            {
+                var test = new TestCase("Tc3_Motion.DriveGeneric_Test.ShouldReturnErrorCode", TestExecutor.ExecutorUri, source);
+                test.LineNumber = 0;
+                test.CodeFilePath = "DriveGeneric_Test.TcPOU";
+                test.DisplayName = "Tc3_Motion.DriveGeneric_Test.ShouldReturnErrorCode";
+             
+                tests.Add(test);
+
+                if (discoverySink != null)
+                {
+                    discoverySink.SendTestCase(test);
+                }
+
+                var test2 = new TestCase("Tc3_Motion.AxisGeneric_Test.ShouldReturnErrorCode", TestExecutor.ExecutorUri, source);
+                test2.LineNumber = 0;
+                test2.CodeFilePath = "AxisGeneric_Test.TcPOU";
+                test2.DisplayName = "Tc3_Motion.AxisGeneric_Test.Should_ThrowInvalidData_When_AxisIdIsZero";
+
+                tests.Add(test2);
+
+                if (discoverySink != null)
+                {
+                    discoverySink.SendTestCase(test2);
+                }
+            }
+
+            return tests;
+        }
+    }
+}
---
+++ src/TcUnit.TestAdapter/TestExecutor.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace TcUnit.TestAdapter
+{
+    [ExtensionUri(TestExecutor.ExecutorUriString)]
+    public class TestExecutor : ITestExecutor
+    {
+        public const string ExecutorUriString = "executor://TcUnitTestExecutor";
+        public static readonly Uri ExecutorUri = new Uri(ExecutorUriString);
+
+        private bool _isCancelled;
+        public void Cancel()
+        {
+            _isCancelled = true;
+        }
+
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            try
+            {
+                foreach(TestCase test in tests)
+                {
+                    if(_isCancelled)
+                    {
+                        break;
+                    }
+
+                    frameworkHandle.RecordStart(test);
+                    frameworkHandle.SendMessage(TestMessageLevel.Informational, "Starting external test for " + test.DisplayName);
+                    var testOutcome = RunExternalTest(test, runContext, frameworkHandle);
+                    frameworkHandle.RecordResult(testOutcome);
+                    frameworkHandle.SendMessage(TestMessageLevel.Informational, "Test result: " + testOutcome.Outcome);
+                }
+            }
+            catch (Exception ex)
+            {
+                frameworkHandle.SendMessage(TestMessageLevel.Error, ex.Message);
+            }
+        }
+
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            try
+            {
+                frameworkHandle.SendMessage(TestMessageLevel.Informational, "Run Unit Tests");
+
+                IEnumerable<TestCase> tests = TestDiscovery.GetTests(sources, null);
+
+                foreach (var test in tests)
+                {
+                    frameworkHandle.SendMessage(TestMessageLevel.Informational, test.DisplayName);
+                }
+
+                RunTests(tests, runContext, frameworkHandle);
+            }
+            catch (Exception ex)
+            {
+                frameworkHandle.SendMessage(TestMessageLevel.Error, ex.Message);
+            }
+        }
+
+        private TestResult RunExternalTest (TestCase test, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            var result = new TestResult(test);
+
+            result.Outcome = TestOutcome.Passed;
+            //result.Outcome = TestOutcome.Failed;
+            //result.Outcome = TestOutcome.Skipped;
+
+            // TODO - run tcunit tests
+            // var testRunner = new TcUnitTestRunner();
+
+            //result.ErrorStackTrace = "error stack trace";
+            //result.ErrorMessage = "error message";
+
+            return result;
+        }
+    }
+}

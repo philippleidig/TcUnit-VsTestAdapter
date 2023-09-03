25f9e147e9f0ea309c9425e83538c17e48eb83f6
philippleidig <P.Leidig@beckhoff.com>
2023-09-03 19:23:56 +0200

    tests: add unit tests

---
+++ tests/TcUnit.TestAdapter.Tests/TestDiscovererTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TcUnit.TestAdapter.Discovery;
+
+namespace TcUnit.TestAdapter.Tests
+{
+    public class TestDiscovererTests : TestContainer
+    {
+
+    }
+}
---
+++ tests/TcUnit.TestAdapter.Tests/TestExecutorTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcUnit.TestAdapter.Tests
+{
+    internal class TestExecutorTests : TestContainer
+    {
+    }
+}

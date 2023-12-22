679c18375343a23e85a26b235cdbf14b53e44380
PhilippLe <45693860+philippleidig@users.noreply.github.com>
2023-12-22 21:43:27 +0100

    feat: add test run cancellation (#16)
    Co-authored-by: philippleidig <P.Leidig@beckhoff.com>

---
+++ src/TcUnit.TestAdapter/Execution/TestRunCancellationToken.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TcUnit.TestAdapter.Execution
+{
+    public class TestRunCancellationToken
+    {
+        private bool _canceled;
+        private Action _registeredCallback;
+
+        public bool Canceled
+        {
+            get => _canceled;
+
+            private set
+            {
+                _canceled = value;
+                if (_canceled)
+                {
+                    _registeredCallback?.Invoke();
+                }
+            }
+        }
+
+        public void Cancel()
+        {
+            Canceled = true;
+        }
+
+        public void Register(Action callback)
+        {
+            _ = callback ?? throw new ArgumentNullException(nameof(callback));
+            _registeredCallback = callback;
+        }
+
+        public void Unregister()
+        {
+            _registeredCallback = null;
+        }
+    }
+}

6090780718edd01d5c75ef4d9a4a2a178f96a1fb
philippleidig <P.Leidig@beckhoff.com>
2023-12-22 19:33:02 +0100

    remove unused usings

--- src/TcUnit.TestAdapter/Common/RTPlatform.cs
+++ src/TcUnit.TestAdapter/Common/RTPlatform.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TcUnit.TestAdapter.Common
+﻿namespace TcUnit.TestAdapter.Common
 {
     public enum RTPlatform
     {
